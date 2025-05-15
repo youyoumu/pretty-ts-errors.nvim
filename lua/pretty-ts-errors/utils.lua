3155e2d6d783b464ce4e9ae3469c3baf021e11c9
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-05-15 19:56:44 +0700

    Handle tables log

--- lua/pretty-ts-errors/utils.lua
+++ lua/pretty-ts-errors/utils.lua
@@ -5,7 +5,13 @@ function M.log_to_file(msg)
 	local log_file = "/tmp/nvim_plugin.log" -- Change this path as needed
 	local file = io.open(log_file, "a") -- Open file in append mode
 	if file then
-		file:write(os.date("[%Y-%m-%d %H:%M:%S] ") .. msg .. "\n")
+		local formatted_msg
+		if type(msg) == "table" then
+			formatted_msg = vim.inspect(msg)
+		else
+			formatted_msg = tostring(msg)
+		end
+		file:write(os.date("[%Y-%m-%d %H:%M:%S] ") .. formatted_msg .. "\n")
 		file:close()
 	else
 		vim.api.nvim_echo({ { "Failed to open log file: " .. log_file, "ErrorMsg" } }, true, {})
